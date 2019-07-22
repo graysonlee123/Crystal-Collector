145039f348e8e12d2d2c1b01eb4733891d592462
Grayson Gantek <graysonleegantek@gmail.com>
2019-07-21 22:16:03 -0400

    Added finishing code touches and begin styling

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -2,6 +2,9 @@ let goal = 0;
 let goalMaxVal = 120;
 let goalMinVal = 19;
 
+let wins = 0;
+let losses = 0;
+
 let crystalMaxVal = 12;
 let crystalMinVal = 1;
 
@@ -19,10 +22,36 @@ game = {
         crystal1Val = Math.floor(Math.random() * (crystalMaxVal - crystalMinVal)) + crystalMinVal;
         crystal2Val = Math.floor(Math.random() * (crystalMaxVal - crystalMinVal)) + crystalMinVal;
         crystal3Val = Math.floor(Math.random() * (crystalMaxVal - crystalMinVal)) + crystalMinVal;
-        crystal4Cal = Math.floor(Math.random() * (crystalMaxVal - crystalMinVal)) + crystalMinVal;
+        crystal4Val = Math.floor(Math.random() * (crystalMaxVal - crystalMinVal)) + crystalMinVal;
 
         userGuess = 0;
 
         $("#randomNumberBox").text(goal);
+    },
+
+    checkForWin: function () {
+        if (userGuess == goal) {
+            game.win();
+        } else if (userGuess > goal) {
+            game.lose();
+        } else {
+            console.log("No win or loss");
+        }
+    }, 
+
+    win: function() {
+        game.initialize();
+        wins ++;
+        $("#gameEndMessage").text("You won the last round!");
+        $("#winsDisplay").text("Wins: " + wins);
+        $("#userScoreDisplay").text("0");
+    },
+
+    lose: function () {
+        game.initialize();
+        losses ++;
+        $("#gameEndMessage").text("You lost the last round!");
+        $("#lossesDisplay").text("Losses: " + losses);
+        $("#userScoreDisplay").text("0");
     }
 }
